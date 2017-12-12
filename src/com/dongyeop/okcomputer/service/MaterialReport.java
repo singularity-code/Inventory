93f5457dfdcc4320c2b190484904daa8233a4e80
Chris <chris.lee@koi.edu.au>
2017-12-13 02:08:40 +1100

    # Report Service Improvement

--- src/com/dongyeop/okcomputer/service/MaterialReport.java
+++ src/com/dongyeop/okcomputer/service/MaterialReport.java
@@ -2,7 +2,11 @@ package com.dongyeop.okcomputer.service;
 
 import java.util.HashMap;
 
+import com.dongyeop.okcomputer.dao.DaoMaterialInterface;
+import com.dongyeop.okcomputer.dto.KoiMaterial;
+
 public interface MaterialReport {
+	HashMap<String, Integer> generateStandardReportMap(HashMap<String, Integer> map, DaoMaterialInterface<KoiMaterial, String> dao);
 	HashMap<String, Integer> selectTotalTvReport();
 	HashMap<String, Integer> selectTotalBarcodeReaderReport();
 	HashMap<String, Integer> selectTotalProjectorReport();
--- src/com/dongyeop/okcomputer/service/MaterialReportImple.java
+++ src/com/dongyeop/okcomputer/service/MaterialReportImple.java
@@ -20,58 +20,8 @@ public class MaterialReportImple implements MaterialReport {
 	@Autowired private DaoMaterialInterface daoMaterialMicrowave;
 	
 	@Autowired private DaoComputerInterface daoComputer;
-	
-	public HashMap<String, Integer> selectTotalTvReport() {
-		HashMap<String, Integer> tvReportMap = new HashMap<String, Integer>();
-		tvReportMap.put("marketStaff", daoMaterialTv.selectTotalMarketStaff());
-		tvReportMap.put("marketStudent", daoMaterialTv.selectTotalMarketStudent());
-		tvReportMap.put("kentL1Staff", daoMaterialTv.selectTotalKentL1Staff());
-		tvReportMap.put("kentL1Student", daoMaterialTv.selectTotalKentL1Student());
-		tvReportMap.put("kentL5Staff",  daoMaterialTv.selectTotalKentL5Staff());
-		tvReportMap.put("kentL5Student", daoMaterialTv.selectTotalKentL5Student());
-		tvReportMap.put("total", daoMaterialTv.getListTotal());
-		return tvReportMap;
-	}
 
 	@Override
-	public HashMap<String, Integer> selectTotalBarcodeReaderReport() {
-		HashMap<String, Integer> barcodeReaderMap = new HashMap<String, Integer>();
-		barcodeReaderMap.put("marketStaff", daoMaterialBarcodeReader.selectTotalMarketStaff());
-		barcodeReaderMap.put("marketStudent", daoMaterialBarcodeReader.selectTotalMarketStudent());
-		barcodeReaderMap.put("kentL1Staff", daoMaterialBarcodeReader.selectTotalKentL1Staff());
-		barcodeReaderMap.put("kentL1Student", daoMaterialBarcodeReader.selectTotalKentL1Student());
-		barcodeReaderMap.put("kentL5Staff",  daoMaterialBarcodeReader.selectTotalKentL5Staff());
-		barcodeReaderMap.put("kentL5Student", daoMaterialBarcodeReader.selectTotalKentL5Student());
-		barcodeReaderMap.put("total", daoMaterialBarcodeReader.getListTotal());
-		return barcodeReaderMap;
-	}
-
-	@Override
-	public HashMap<String, Integer> selectTotalProjectorReport() {
-		HashMap<String, Integer> projectorMap = new HashMap<String, Integer>();
-		projectorMap.put("marketStaff", daoMaterialProjector.selectTotalMarketStaff());
-		projectorMap.put("marketStudent", daoMaterialProjector.selectTotalMarketStudent());
-		projectorMap.put("kentL1Staff", daoMaterialProjector.selectTotalKentL1Staff());
-		projectorMap.put("kentL1Student", daoMaterialProjector.selectTotalKentL1Student());
-		projectorMap.put("kentL5Staff",  daoMaterialProjector.selectTotalKentL5Staff());
-		projectorMap.put("kentL5Student", daoMaterialProjector.selectTotalKentL5Student());
-		projectorMap.put("total", daoMaterialProjector.getListTotal());
-		return projectorMap;
-	}
-
-	@Override
-	public HashMap<String, Integer> selectTotalMicrowaveReport() {
-		HashMap<String, Integer> microwaveMap = new HashMap<String, Integer>();
-		microwaveMap.put("marketStaff", daoMaterialMicrowave.selectTotalMarketStaff());
-		microwaveMap.put("marketStudent", daoMaterialMicrowave.selectTotalMarketStudent());
-		microwaveMap.put("kentL1Staff", daoMaterialMicrowave.selectTotalKentL1Staff());
-		microwaveMap.put("kentL1Student", daoMaterialMicrowave.selectTotalKentL1Student());
-		microwaveMap.put("kentL5Staff",  daoMaterialMicrowave.selectTotalKentL5Staff());
-		microwaveMap.put("kentL5Student", daoMaterialMicrowave.selectTotalKentL5Student());
-		microwaveMap.put("total", daoMaterialMicrowave.getListTotal());
-		return microwaveMap;
-	}
-	
 	public HashMap<String, Integer> generateStandardReportMap(HashMap<String, Integer> map, DaoMaterialInterface<KoiMaterial, String> dao) {
 		map.put("marketStaff", dao.selectTotalMarketStaff());
 		map.put("marketStudent", dao.selectTotalMarketStudent());
@@ -81,5 +31,33 @@ public class MaterialReportImple implements MaterialReport {
 		map.put("kentL5Student", dao.selectTotalKentL5Student());
 		map.put("total", dao.getListTotal());
 		return map;
+	}
+	
+	@Override
+	public HashMap<String, Integer> selectTotalTvReport() {
+		HashMap<String, Integer> tvReportMap = new HashMap<String, Integer>();
+		generateStandardReportMap(tvReportMap, daoMaterialTv);
+		return tvReportMap;
+	}
+
+	@Override
+	public HashMap<String, Integer> selectTotalBarcodeReaderReport() {
+		HashMap<String, Integer> barcodeReaderMap = new HashMap<String, Integer>();
+		generateStandardReportMap(barcodeReaderMap, daoMaterialBarcodeReader);
+		return barcodeReaderMap;
+	}
+
+	@Override
+	public HashMap<String, Integer> selectTotalProjectorReport() {
+		HashMap<String, Integer> projectorMap = new HashMap<String, Integer>();
+		generateStandardReportMap(projectorMap, daoMaterialProjector);
+		return projectorMap;
+	}
+
+	@Override
+	public HashMap<String, Integer> selectTotalMicrowaveReport() {
+		HashMap<String, Integer> microwaveMap = new HashMap<String, Integer>();
+		generateStandardReportMap(microwaveMap, daoMaterialMicrowave);
+		return microwaveMap;
 	}
 }
